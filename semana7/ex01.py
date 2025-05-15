2e5e60b9130c7e62b965395b0708e08a9a3450a8
Rafael <eduardo.suarez@amf.edu.br>
2025-05-15 14:50:00 -0300

    Update ex01.py

--- semana7/ex01.py
+++ semana7/ex01.py
@@ -1,24 +1,12 @@
-lista = []
-lista1 = []
-count = 0
-soma = 0
-soma1 = 0
-
-
-while count != 8:
-    num1 = int(input("digite um numero: "))
-    lista.append(num1)
-    count += 1
-    soma += num1
-
-
-    if num1 > 30:
-        lista1.append(num1)
-        soma1 += num1
+lista = ["cachorro", "gato","papagaio","tartaruga"]
+lista[1] = 'coelho'
 print(lista)
-print(soma)
-print(lista1)
-print(soma1)
+print(lista[0])
+print(lista[3])
+
+
+
+
 
 
 
